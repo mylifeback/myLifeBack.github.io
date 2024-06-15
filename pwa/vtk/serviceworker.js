22e7f641b1f9d0c5bc4a1c8f93a8fb138001cec8
m's experiments <mauricewong@me.com>
2024-06-15 20:47:06 +0800

    s

--- pwa/vtk/serviceworker.js
+++ pwa/vtk/serviceworker.js
@@ -1,28 +1,24 @@
 var staticCacheName = "pwa";
 
-// const assets = [
-//   "manifest.json",
-//   "serviceworker.js",
-//   "index.html",
-//   "src/main.css",
-//   "src/main.js",
-//   "src/favicon.ico",
-//   "src/icon-192x192.png",
-//   "src/icon-512x512.png",
-//   "src/fflate.module.js",
-//   "src/lil-gui-module.min.js",
-//   "src/NRRDLoader.js",
-//   "src/stats.module.js",
-//   "src/three.js",
-//   "src/TrackballControls.js",
-//   "src/VTKLoader.js"
-// ];
-
 const assets = [
-
+  "manifest.json",
+  "serviceworker.js",
+  "index.html",
+  "src/main.css",
+  "src/main.js",
+  "src/favicon.ico",
+  "src/icon-192x192.png",
+  "src/icon-512x512.png",
+  "src/fflate.module.js",
+  "src/lil-gui-module.min.js",
+  "src/NRRDLoader.js",
+  "src/stats.module.js",
+  "src/three.js",
   "src/TrackballControls.js",
   "src/VTKLoader.js"
 ];
+
+
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
