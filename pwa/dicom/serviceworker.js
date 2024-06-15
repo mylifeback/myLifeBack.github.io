d313769df067c9a27244d263b7b28c53d464e12f
m's experiments <mauricewong@me.com>
2024-06-15 20:36:01 +0800

    d

--- pwa/dicom/serviceworker.js
+++ pwa/dicom/serviceworker.js
@@ -1,22 +1,26 @@
 var staticCacheName = "pwa";
 
+// const assets = [
+//   "manifest.json",
+//   "serviceworker.js",
+//   "index.html",
+//   "src/main.css",
+//   "src/main.js",
+//   "src/favicon.ico",
+//   "src/icon-192x192.png",
+//   "src/icon-512x512.png",
+//   "src/fflate.module.js",
+//   "src/lil-gui-module.min.js",
+//   "src/NRRDLoader.js",
+//   "src/stats.module.js",
+//   "src/three.js",
+//   "src/TrackballControls.js",
+//   "src/Volume.js",
+//   "src/VolumeSlice.js"
+// ];
+
 const assets = [
-  "manifest.json",
-  "serviceworker.js",
-  "index.html",
-  "src/main.css",
-  "src/main.js",
-  "src/favicon.ico",
-  "src/icon-192x192.png",
-  "src/icon-512x512.png",
-  "src/fflate.module.js",
-  "src/lil-gui-module.min.js",
-  "src/NRRDLoader.js",
-  "src/stats.module.js",
-  "src/three.js",
-  "src/TrackballControls.js",
-  "src/Volume.js",
-  "src/VolumeSlice.js"
+  "manifest.json"
 ];
 
 self.addEventListener("install", function(e) {
