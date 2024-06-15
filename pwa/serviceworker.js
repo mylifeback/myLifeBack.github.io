43a409d18f8288cf6b3d9683ee89b05eea6d3e54
ma <ma>
2024-06-15 16:37:42 +0800

    update

---
+++ pwa/serviceworker.js
@@ -0,0 +1,27 @@
+var staticCacheName = "pwa";
+
+const assets = [
+  "/",
+  "/index.html",
+  "src/favicon.ico",
+  "src/icon-192x192.png",
+  "src/icon-512x512.png",
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
