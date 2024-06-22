35c819016fafd588f1d5906acb0fbf6298fb4be8
m's experiments <mauricewong@me.com>
2024-06-22 09:02:20 +0800

    d

---
+++ lab/pwa/phone/serviceworker.js
@@ -0,0 +1,30 @@
+var staticCacheName = "pwa";
+
+const assets = [
+  "manifest.json",
+  "serviceworker.js",
+  "index.html",
+  "src/main.css",
+  "src/main.js",
+  "src/favicon.ico",
+  "src/icon-192x192.png",
+  "src/icon-512x512.png"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
