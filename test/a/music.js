e957d27480cff6031d753bc5fa602ecba6c66ae5
m's experiments <mauricewong@me.com>
2024-07-27 18:51:17 +0800

    d

--- test/a/music.js
+++ test/a/music.js
@@ -1,11 +1,28 @@
 AFRAME.registerComponent('music', {
   init: function () {
     console.log ('music component init');
-    import './src/howler.min.js';
+    import ('./src/howler.min.js');
 
+    this.sound = new Howl({
+      src: ['./src/world.mp3'],
+      sprite: {
+          first: [2000, 2000],
+          second: [4000, 1000],
+          third: [5000, 3000],
+          fourth: [8000, 5000]
+      }
+    });
+
+    window.addEventListener ('gamepad', this.play(fourth).bind(this));
+    console.log ('listener added');
   },
 
+  play: function (part) {
 
+      this.sound.play(part);
+      console.log('is it playing??  ' + part);
+  
+  },
 
   tick: function (time, timeDelta) {
 
