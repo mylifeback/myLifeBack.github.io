d1f6cbbcaed2f26297fdad7a004574f32d59da4c
m's experiments <mauricewong@me.com>
2024-08-01 17:07:16 +0800

    s

--- test/webcomponents/webcomponents.js
+++ test/webcomponents/webcomponents.js
@@ -29,5 +29,5 @@ customElements.define("service-worker", ServiceWorker);
 
 function kicking (){
   console.log('kicking');
-  document.getElementsByTagName('service-worker').setAttribute('color', 'blueorigin')
+  document.getElementsByTagName('service-worker').setAttribute('color', 'blueorigin');
 }
