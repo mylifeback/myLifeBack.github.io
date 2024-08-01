76142155fc1d27baf7d2a074acb1340bb8470cbe
m's experiments <mauricewong@me.com>
2024-08-01 15:57:19 +0800

    e

---
+++ test/webcomponent/webcomponents.js
@@ -0,0 +1,26 @@
+class ServiceWorker extends HTMLElement {
+
+  static observedAttributes = ["color", "size"];
+
+  constructor() {
+    super();
+  }
+  // Element functionality written in 
+  connectedCallback() {
+    console.log("Custom element added to page.");
+  }
+
+  disconnectedCallback() {
+    console.log("Custom element removed from page.");
+  }
+
+  adoptedCallback() {
+    console.log("Custom element moved to new page.");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute ${name} has changed.`);
+  }
+}
+
+customElements.define("service-worker", ServiceWorker);
