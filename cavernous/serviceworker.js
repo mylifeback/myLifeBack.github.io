497555b10e23e7b4a4bb97485af258f32d2409cf
m's experiments <mauricewong@me.com>
2024-09-19 18:52:50 +0800

    d

---
+++ cavernous/serviceworker.js
@@ -0,0 +1,33 @@
+var staticCacheName = "skull";
+
+const assets = [
+  "/",
+  "manifest.json",
+  "serviceworker.js",
+  "index.html",
+  "src/main.css",
+  "src/main.js",
+  "src/favicon.ico",
+  "src/icon-192x192.png",
+  "src/icon-512x512.png",
+  "file/skin.vtk",
+  "file/lesion.vtk"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
