2c767bbc1d5a1f53dad67cea58db2b4e738ffcd5
m's experiments <mauricewong@me.com>
2024-10-17 18:39:38 +0800

    d

--- meningioma/models.js
+++ meningioma/models.js
@@ -1,2 +1,2 @@
-const models = [["lesion", "red", 1.0], ["skin", "green", 0.7], ["dura", "blue", 0.8, "1"], ["opening", "purple", 0.8, "2"], ["half", "purple", 0.8, "3"], ["marker", 0xFFFFFF, 0.8, "4"]];
+const models = [["lesion", "red", 1.0], ["skin", "green", 0.7], ["dura", "blue", 0.8, "1"], ["opening", "purple", 0.8, "2"], ["half", "green", 0.8, "3"], ["marker", 0xFFFFFF, 0.8, "4"]];
 export default models;
