e6aacfd253dad4c2bdba487770e2838a72865c15
m's experiments <mauricewong@me.com>
2024-10-21 16:39:49 +0800

    s

--- future/names.js
+++ future/names.js
@@ -1,2 +1,6 @@
-const names = {title: "aneurysm", topic: "cavernous segment aneurysm"};
+const names = {
+  title: "aneurysm",
+  topic: "cavernous segment aneurysm",
+  file: "./file/dicom.nrrd"
+};
 export default names;
